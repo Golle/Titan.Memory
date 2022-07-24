f49e209803a79a602484d08a41d50d75bddb68c6
Jens Andersson <jens.andersson5@gmail.com>
2022-07-24 06:19:41 +0200

    Add LinearArena

---
+++ src/Titan.Memory/LinearArena.cs
@@ -0,0 +1,85 @@
+﻿using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Titan.Memory;
+
+
+internal static class LinearArenaExtensions
+{
+    public static unsafe T* Allocate<T>(this ref LinearArena arena) where T : unmanaged => (T*)arena.Allocate((nuint)sizeof(T));
+}
+
+internal unsafe struct LinearArena
+{
+    private static readonly nuint FooterSize = (nuint)sizeof(Footer);
+    private readonly PlatformAllocator* _allocator;
+    private readonly byte* _baseMemory;
+    private byte* _current;
+    private volatile int _offset; // the memory offset in the current region
+    private readonly int _blockSize; // A block is the entire region of the memory
+
+    public void* Allocate(nuint size)
+    {
+        var alignedSize = (int)size; // Add alignment
+        if (alignedSize + _offset >= _blockSize)
+        {
+            Expand();
+        }
+        var offset = Interlocked.Add(ref _offset, alignedSize) - alignedSize; // Increment the _offset and subtract the size to get the start of the block
+        var ptr = _current + offset;
+        Console.WriteLine($"Allocate: {(nuint)ptr}");
+        return ptr;
+    }
+
+    public void Reset()
+    {
+        // Reset the offset and set the current memory pointer to the current block
+        //NOTE(Jens): Should we release any newly allocated blocks or just re-use them when/if needed again?
+        _offset = 0;
+        _current = _baseMemory;
+        var footer = GetFooter(_baseMemory);
+        RecursiveRelease(footer->Next);
+        footer->Next = null;
+    }
+
+    private void RecursiveRelease(byte* current)
+    {
+        while (current != null)
+        {
+            var next = GetFooter(current)->Next;
+            Console.WriteLine($"Free: {(nuint)current}");
+            _allocator->Free(current);
+            current = next;
+        }
+    }
+
+    private LinearArena(PlatformAllocator* allocator, nuint blockSize)
+    {
+        _allocator = allocator;
+        _blockSize = (int)blockSize;
+        _offset = 0;
+        _current = _baseMemory = (byte*)_allocator->Allocate(blockSize + FooterSize); // TODO: Align this
+    }
+
+    public static LinearArena Create(PlatformAllocator* allocator, nuint initialSize) => new(allocator, initialSize);
+
+    private void Expand()
+    {
+        _offset = 0;
+        var mem = (byte*)_allocator->Allocate((nuint)_blockSize + FooterSize);
+        GetFooter(_current)->Next = mem;
+        _current = mem;
+        Console.WriteLine($"Expanding the pool: {(nuint)_current}");
+    }
+
+    public void Release() 
+        => RecursiveRelease(_baseMemory);
+
+    //NOTE(Jens): The footer is used to track the info we need for new blocks
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private Footer* GetFooter(byte* ptr) => (Footer*)(ptr + _blockSize);
+    private struct Footer
+    {
+        public byte* Next;
+    }
+}
--- src/Titan.Memory/Program.cs
+++ src/Titan.Memory/Program.cs
@@ -5,38 +5,91 @@ unsafe
 
     var native = PlatformAllocator.Create<NativeMemoryAllocator>();
     var win32 = PlatformAllocator.Create<Win32VirtualAllocAllocator>();
-
-    var arena = PoolArena.Create(&win32, 10, (uint)sizeof(TestStruct));
-
-    var maxCount = 12;
-    var test = stackalloc TestStruct*[maxCount];
-    for (var i = 0; i < maxCount; ++i)
     {
-        var test1 = arena.Allocate<TestStruct>();
-        test1->A = 10 * i;
-        test1->B = 12 * i;
-        test1->C = 13 * i;
-        test[i] = test1;
-    }
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"****** {nameof(PoolArena)} ******");
+        Console.ResetColor();
+        Console.WriteLine($"{nameof(PoolArena)} size {sizeof(PoolArena)} bytes");
+        var arena = PoolArena.Create(&win32, 10, (uint)sizeof(TestStruct));
 
-    for (var i = 0; i < 5; ++i)
-    {
-        arena.Free(test[i]);
-    }
-    for (var i = 0; i < 5; ++i)
-    {
-        var test1 = arena.Allocate<TestStruct>();
-        test1->A = 2 * i;
-        test1->B = 3 * i;
-        test1->C = 4 * i;
-        test[i] = test1;
-    }
-    for (var i = 0; i < 10; ++i)
-    {
-        Console.WriteLine($"Test1 {test[i]->A} {test[i]->B} {test[i]->C}");
-    }
+        var maxCount = 12;
+        var test = stackalloc TestStruct*[maxCount];
+        for (var i = 0; i < maxCount; ++i)
+        {
+            var test1 = arena.Allocate<TestStruct>();
+            test1->A = 10 * i;
+            test1->B = 12 * i;
+            test1->C = 13 * i;
+            test[i] = test1;
+        }
 
-    arena.Release();
+        for (var i = 0; i < 5; ++i)
+        {
+            arena.Free(test[i]);
+        }
+        for (var i = 0; i < 5; ++i)
+        {
+            var test1 = arena.Allocate<TestStruct>();
+            test1->A = 2 * i;
+            test1->B = 3 * i;
+            test1->C = 4 * i;
+            test[i] = test1;
+        }
+        for (var i = 0; i < 10; ++i)
+        {
+            Console.WriteLine($"Pool Arena[{i}]: {test[i]->A} {test[i]->B} {test[i]->C}");
+        }
+
+        arena.Release();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"****** {nameof(PoolArena)} ******");
+        Console.ResetColor();
+    }
+    Console.WriteLine();
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"****** {nameof(LinearArena)} ******");
+        Console.ResetColor();
+        // linear arena
+        Console.WriteLine($"{nameof(LinearArena)} size {sizeof(LinearArena)} bytes");
+        var arena = LinearArena.Create(&win32, 320);
+        var maxCount = 12;
+        var test = stackalloc TestStruct*[maxCount];
+        for (var i = 0; i < maxCount; ++i)
+        {
+            var value = arena.Allocate<TestStruct>();
+            value->A = 10 * i;
+            value->B = 12 * i;
+            value->C = 13 * i;
+            test[i] = value;
+        }
+        for (var i = 0; i < 10; ++i)
+        {
+            Console.WriteLine($"Linear Arena[{i}]: {test[i]->A} {test[i]->B} {test[i]->C}");
+        }
+        arena.Reset();
+        for (var i = 0; i < maxCount*3; ++i)
+        {
+            var value = arena.Allocate<TestStruct>();
+            value->A = 10 * i;
+            value->B = 12 * i;
+            value->C = 13 * i;
+        }
+        //arena.Reset(); 
+        
+        for (var i = 0; i < maxCount; ++i)
+        {
+            var value = arena.Allocate<TestStruct>();
+            value->A = 10 * i;
+            value->B = 12 * i;
+            value->C = 13 * i;
+            test[i] = value;
+        }
+        arena.Release();
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"****** {nameof(LinearArena)} ******");
+        Console.ResetColor();
+    }
 
     var mem1 = (byte*)native.Allocate(1024);
     var mem2 = (byte*)win32.Allocate(1024);
@@ -58,6 +111,5 @@ internal struct TestStruct
 }
 
 
-internal struct LinearArena { /*TBD*/ }
 internal unsafe struct FixedSizeArena { /*TBD*/ }
 internal unsafe struct FixedPoolSizeArea { /*TBD*/ }
